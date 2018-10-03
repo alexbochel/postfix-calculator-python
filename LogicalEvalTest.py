0b1eed6bc870f8e9af29ad0fc9432ba08a3175c4
Alex Bochel <alexbochel@Alexs-MacBook-Air.local>
2018-10-03 19:05:19 -0400

    Moved LogicalEvalTest to a folder that is recognized by git

---
+++ LogicalEvalTest.py
@@ -0,0 +1,11 @@
+import unittest
+from PostFixProject import LogicalEval
+
+class TestPostFixCalculator(unittest.TestCase):
+
+    def setUp(self):
+        logical_evaluator = LogicalEval()
+
+
+if __name__ == '__main__':
+        unittest.main()
