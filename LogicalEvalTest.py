e1da68b76bde4744c8bdc5fb93593bbfecd0215f
Alex Bochel <alexbochel@Alexs-MacBook-Air.local>
2018-10-03 21:42:22 -0400

    Added tests for 'or' method

--- LogicalEvalTest.py
+++ LogicalEvalTest.py
@@ -83,6 +83,61 @@ class TestLogicalEval(unittest.TestCase):
         # Assert
         self.assertEquals(expected_char, actual_char)
 
+    def test_or_true_ones(self):
+        # Arrange
+        logical_evaluator = LogicalEval('11|')
+        expected_char = '1'
+
+        # Act
+        actual_char = logical_evaluator.Process_Expression()
+
+        # Assert
+        self.assertEquals(expected_char, actual_char)
+
+    def test_or_true_one_and_zero(self):
+        # Arrange
+        logical_evaluator = LogicalEval('01|')
+        expected_char = '1'
+
+        # Act
+        actual_char = logical_evaluator.Process_Expression()
+
+        # Assert
+        self.assertEquals(expected_char, actual_char)
+
+    def test_or_false(self):
+        # Arrange
+        logical_evaluator = LogicalEval('00|')
+        expected_char = '0'
+
+        # Act
+        actual_char = logical_evaluator.Process_Expression()
+
+        # Assert
+        self.assertEquals(expected_char, actual_char)
+
+    # def test_not_equals_true(self):
+    #     # Arrange
+    #     logical_evaluator = LogicalEval('11=')
+    #     expected_char = '1'
+    #
+    #     # Act
+    #     actual_char = logical_evaluator.Process_Expression()
+    #
+    #     # Assert
+    #     self.assertEquals(expected_char, actual_char)
+    #
+    # def test_not_equals_false(self):
+    #     # Arrange
+    #     logical_evaluator = LogicalEval('10=')
+    #     expected_char = '0'
+    #
+    #     # Act
+    #     actual_char = logical_evaluator.Process_Expression()
+    #
+    #     # Assert
+    #     self.assertEquals(expected_char, actual_char)
+
 
 if __name__ == '__main__':
         unittest.main()
