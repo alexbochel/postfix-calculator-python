f82ccf694d9712f5bf4abc218c679f6ba5572e46
Alex Bochel <alexbochel@Alexs-MacBook-Air.local>
2018-10-17 17:10:49 -0400

    Modified file to not need stack class

--- PostFixProject.py
+++ PostFixProject.py
@@ -2,30 +2,23 @@ from __future__ import print_function
 
 PF1 = "0!!!1/10&1!!&=1=0|"
 PF2 = "10=!10/&11!0/=1|11!&|&0/"
+TOS = 0
 
-class Stack:
-
-    def __init__(self):
-        self.stack = list()
-        self.index = -1
-
-    def Push(self, element):
-        self.stack.append(element)
-        self.index += 1
-
-    def Pop(self):
-        item = self.stack[self.index]
-        self.stack.reverse()
-        self.stack.remove(item) #Takes from beginning of the list not the end
-        self.stack.reverse()
-        self.index -= 1
-        return item
 
 class LogicalEval:
-
     def __init__(self, expression):
-        self.stack = Stack()
+        self.stack = [0] * 30
         self.expression = expression
+        self.counter = 0
+
+    def Push(self, element):
+        self.stack[self.counter] = element
+        self.counter += 1
+
+    def Pop(self):
+        self.counter -= 1
+        item = self.stack[self.counter]
+        return item
 
     def Not(self, expression):
         if (expression == '1'):
@@ -60,24 +53,24 @@ class LogicalEval:
     def Process_Expression(self):
         for character in self.expression:
             if (character == '1') or (character == '0'):
-                self.stack.Push(character)
+                self.Push(character)
             elif (character == '='):
-                new_value = self.Equal(self.stack.Pop(), self.stack.Pop())
-                self.stack.Push(new_value)
+                new_value = self.Equal(self.Pop(), self.Pop())
+                self.Push(new_value)
             elif (character == '!'):
-                new_value = self.Not(self.stack.Pop())
-                self.stack.Push(new_value)
+                new_value = self.Not(self.Pop())
+                self.Push(new_value)
             elif (character == '&'):
-                new_value = self.And(self.stack.Pop(), self.stack.Pop())
-                self.stack.Push(new_value)
+                new_value = self.And(self.Pop(), self.Pop())
+                self.Push(new_value)
             elif (character == '|'):
-                new_value = self.Or(self.stack.Pop(), self.stack.Pop())
-                self.stack.Push(new_value)
+                new_value = self.Or(self.Pop(), self.Pop())
+                self.Push(new_value)
             elif (character == '/'):
-                new_value = self.Not_Equal(self.stack.Pop(), self.stack.Pop())
-                self.stack.Push(new_value)
+                new_value = self.Not_Equal(self.Pop(), self.Pop())
+                self.Push(new_value)
 
-        return self.stack.Pop()
+        return self.stack[TOS]
 
 if __name__ == '__main__':
     log = open("output.txt", "w")
